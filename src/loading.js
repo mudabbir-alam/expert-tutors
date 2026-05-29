ad6b5989f40d7ee571f41f1b55a228a472cfa956
mudabbir-alam <mudabbiralam885@gmail.com>
2026-05-30 00:17:35 +0500

    Added Skeleton Screen

--- src/loading.js
+++ src/loading.js
@@ -1,9 +1,19 @@
-window.addEventListener("load", () => {
-    const skeleton = document.getElementById("skeleton");
-    const app = document.getElementById("app");
+window.addEventListener("load", async () => {
+    try {
+        await Promise.all([
+            document.fonts.load('400 1em "Material Icons"'),
+            document.fonts.load('500 1em "Material Icons"'),
+            document.fonts.load('600 1em "Material Icons"'),
+            document.fonts.load('700 1em "Material Icons"'),
+        ]);
 
-    setTimeout(() => {
-        skeleton.classList.add("hidden");
+        await new Promise((resolve) => setTimeout(resolve, 300));
+
+        const skeleton = document.getElementById("skeleton");
+        const app = document.getElementById("app");
+        skeleton.remove();
         app.classList.remove("hidden");
-    }, 200);
+    } catch (error) {
+        console.log(error);
+    }
 });
