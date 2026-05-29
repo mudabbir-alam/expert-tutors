c7497169d517b90c90cfab2cc1e44da965afa7d2
mudabbir-alam <mudabbiralam885@gmail.com>
2026-05-30 00:29:11 +0500

    Added Skeleton Screen

--- src/loading.js
+++ src/loading.js
@@ -1,11 +1,18 @@
 window.addEventListener("load", async () => {
     try {
-        await Promise.all([
+        const fontPromise = Promise.all([
             document.fonts.load('400 1em "Material Icons"'),
             document.fonts.load('500 1em "Material Icons"'),
             document.fonts.load('600 1em "Material Icons"'),
             document.fonts.load('700 1em "Material Icons"'),
         ]);
+
+        const timeOutPromise = new Promise((resolve) => {
+            setTimeout(resolve, 5000);
+        });
+
+        await Promise.race([fontPromise, timeOutPromise]);
+
 
         await new Promise((resolve) => setTimeout(resolve, 300));
 
